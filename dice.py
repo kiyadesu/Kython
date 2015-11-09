19e11cf77d434e8d9314c45096d58e71135bc689
kiya-z <2267161320@qq.com>
2015-11-09 18:43:41 +0800

    make dice into a iterator

--- dice.py
+++ dice.py
@@ -1,12 +1,15 @@
+#coding:utf-8
 import random
 
 class Dice(object):
 
     sides = ['red','orange','yellow','green','cray','blue']
 
-    def __init__(self,num):
+    def __init__(self,num=1,iterend=6):
+        print 'init > num:%s | iterend:%s' % (num,iterend)
         self.color = self.sides[num]
-        print self.color
+        self.iterend = iterend
+        print 'init > color: ',self.color
 
     @staticmethod
     def calcSum(color1,color2):
@@ -14,9 +17,24 @@ class Dice(object):
 
     @classmethod
     def get_dice(cls):
-        return cls(random.randint(0,6))
+        return cls(num=random.randint(0,5)) # randint(a,b) : a <= x <= b
+
+    def __iter__(self):
+        self.count = 0
+        return self
+
+    def next(self):#shit! in python3, the func name here should be __next__
+        if self.count < self.iterend:
+            self.count += 1
+            self.color = self.sides[random.randint(0,5)]
+            return self.color
+        else:
+            raise StopIteration
 
 if __name__ == '__main__':
     d1 = Dice.get_dice()
     d2 = Dice.get_dice()
     print Dice.calcSum(d1.color,d2.color)
+    print '——————iter——cut——————'
+    for i in Dice(iterend=6):
+        print i
