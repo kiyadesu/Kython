2d8082346769906d48359d5c196b1ba55dab4288
kiya-z <2267161320@qq.com>
2015-11-23 18:54:55 +0800

    add auto-update-webpage

---
+++ gray-hat-python/auto-update-webpage.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+import webbrowser
+import threading
+from sys import exit
+
+def updateKX():
+
+        webbrowser.open('http://bbs.pediy.com',new=0)
+        global t        #Notice: use global variable!
+        t = threading.Timer(300.0, updateKX)
+        t.start()
+        global i
+        i += 1
+        print i
+        if i > 15:
+            print 'exit'
+            t.cancel()
+
+i = 0
+t = threading.Timer(300.0, updateKX)
+t.start()
