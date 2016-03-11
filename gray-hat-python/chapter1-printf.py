d0f4d0f2b17cc2141f75b3758b0b9f15b69a3125
kiya-z <2267161320@qq.com>
2016-03-11 18:14:40 +0800

    scanf

--- gray-hat-python/chapter1-printf.py
+++ gray-hat-python/chapter1-printf.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
+#coding:utf-8
 from ctypes import *
-libc = CDLL("libc.so.6")
-message_string = "Hello world!\n"
-libc.printf("test: %s", message_string)
+libc = CDLL("libc.so.6")    //CDLL -> 加载遵循cdecl标准函数调用约定的链接库
+
+message_string = ""
+libc.scanf("%s",message_string);
+libc.printf("Hello %s\n", message_string)
